5d319f21e5397ddfb08febd6751b15d835d72126
Mae <maya@carrionandon.me>
2023-11-14 20:23:44 -0800

    Move to own melonprefs file

--- Main.cs
+++ Main.cs
@@ -10,18 +10,19 @@ namespace DoubleJump
 {
     public class Main : MelonMod
     {
-        internal const string Name = "Double Jump"; // Required
-        internal const string Description = "Reenables the double jump feature in BONELAB."; // Required
-        internal const string Author = "SoulWithMae"; // Required
-        internal const string Company = "Weather Electric"; // Set as null if blank
-        internal const string Version = "1.0.2"; // Required
-        internal const string DownloadLink = "https://bonelab.thunderstore.io/package/CarrionAndOn/DoubleJump/"; // Set as null if blank
+        internal const string Name = "Double Jump";
+        internal const string Description = "Reenables the double jump feature in BONELAB.";
+        internal const string Author = "SoulWithMae";
+        internal const string Company = "Weather Electric";
+        internal const string Version = "1.0.2";
+        internal const string DownloadLink = "https://bonelab.thunderstore.io/package/CarrionAndOn/DoubleJump/";
         
         public override void OnInitializeMelon()
         {
             SetupBonemenu();
+            ModConsole.Setup(LoggerInstance);
             Preferences.Setup();
-            _enabled = Preferences.autoEnable;
+            _enabled = Preferences.AutoEnable;
         }
         [HarmonyPatch(typeof(Player_Health), "MakeVignette")]
         public static class VignettePatch
@@ -30,33 +31,38 @@ namespace DoubleJump
             {
                 if (_enabled)
                 {
+                    ModConsole.Msg("Enabling double jump automatically", LoggingMode.DEBUG);
                     Player.remapRig.doubleJump = true;
                 }
             }
         }
 
-        private static bool _enabled = false;
+        private static bool _enabled;
         private static void SetupBonemenu()
         {
             MenuCategory mainCat = MenuManager.CreateCategory("Weather Electric", "#6FBDFF");
             MenuCategory menuCategory = mainCat.CreateCategory("Double Jump", "#fe9500");
             menuCategory.CreateFunctionElement("Enable", Color.green, Enable);
             menuCategory.CreateFunctionElement("Disable", Color.red, Disable);
-            menuCategory.CreateBoolElement("Auto Enable", Color.yellow, _enabled, delegate(bool value)
-            {
-                _enabled = value;
-                Preferences.autoEnable.entry.Value = value;
-                Preferences.category.SaveToFile();
-            });
+            menuCategory.CreateBoolElement("Auto Enable", Color.yellow, _enabled, OnBoolChanged);
+        }
+
+        private static void OnBoolChanged(bool value)
+        {
+            _enabled = value;
+            Preferences.AutoEnable.entry.Value = value;
+            Preferences.OwnCategory.SaveToFile();
         }
 
         private static void Enable()
         {
+            ModConsole.Msg("Enabling double jump", LoggingMode.DEBUG);
             Player.remapRig.doubleJump = true;
         }
 
         private static void Disable()
         {
+            ModConsole.Msg("Disabling double jump", LoggingMode.DEBUG);
             Player.remapRig.doubleJump = false;
         }
     }
--- Melon/Preferences.cs
+++ Melon/Preferences.cs
@@ -4,15 +4,22 @@ namespace DoubleJump.Melon
 {
     internal static class Preferences
     {
-        public static MelonPreferences_Category category = MelonPreferences.CreateCategory("Double Jump");
+        public static readonly MelonPreferences_Category GlobalCategory = MelonPreferences.CreateCategory("Global");
+        public static readonly MelonPreferences_Category OwnCategory = MelonPreferences.CreateCategory("Double Jump");
+        
+        public static ModPref<LoggingMode> LoggingMode;
 
-        public static ModPref<bool> autoEnable;
+        public static ModPref<bool> AutoEnable;
 
         public static void Setup()
         {
-            autoEnable = new ModPref<bool>(category, "AutoEnable", false);
-
-            category.SaveToFile(false);
+            AutoEnable = new ModPref<bool>(OwnCategory, "AutoEnable", false);
+            LoggingMode = new ModPref<LoggingMode>(GlobalCategory, "LoggingMode", global::LoggingMode.NORMAL, "Logging Mode", "The level of logging to use. DEBUG = Everything, NORMAL = Important Only");
+            GlobalCategory.SetFilePath(MelonUtils.UserDataDirectory+"/WeatherElectric.cfg");
+            GlobalCategory.SaveToFile(false);
+            OwnCategory.SetFilePath(MelonUtils.UserDataDirectory+"/WeatherElectric.cfg");
+            OwnCategory.SaveToFile(false);
+            ModConsole.Msg("Finished preferences setup for DoubleJump", global::LoggingMode.DEBUG);
         }
     }
 }
